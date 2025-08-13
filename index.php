8323a91ad05086ca765d9edb55d1143e850e061c
Saqaf <saqafaliabdo716@gmail.com>
2025-08-13 06:28:19 +0300

    feat: create form post

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="process.php" method="post">
+        <input type="text" name="name" placeholder="your name">
+        <input type="text" name="color-favorite" placeholder="your favorite color">
+        <input type="submit" value="submit">
+    
+    </form>
+</body>
+</html>
