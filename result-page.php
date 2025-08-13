03796dceef56b46afaedf5e2f6365b2ea4653fc3
Saqaf <saqafaliabdo716@gmail.com>
2025-08-13 08:15:44 +0300

    feat: receive and check data  from form post

--- result-page.php
+++ result-page.php
@@ -1,1 +1,16 @@
 <?php
+
+//  Receive the data from form post
+if (isset($_POST['name']) && isset($_POST['color-favorite'])) {
+    if (!empty($_POST['name']) && !empty($_POST['color-favorite'])) { //Data validation
+        // Data filter
+        $name = htmlspecialchars($_POST['name']);
+        $color = htmlspecialchars($_POST['color-favorite']);
+   
+        // This message will be displayed if the data is not valid
+    } else {
+      
+    }
+} else {
+   
+}
