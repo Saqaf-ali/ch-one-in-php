4de74f2ea0af73a962fdb303c128030996c913c8
Saqaf <saqafaliabdo716@gmail.com>
2025-08-13 08:23:17 +0300

    feat:add text color the same color-favoriteas

--- result-page.php
+++ result-page.php
@@ -6,11 +6,11 @@ if (isset($_POST['name']) && isset($_POST['color-favorite'])) {
         // Data filter
         $name = htmlspecialchars($_POST['name']);
         $color = htmlspecialchars($_POST['color-favorite']);
-   
+        echo "<h1 style='color:$color;' >Hi $name, your favorite color is $color</h1>";// Data display
         // This message will be displayed if the data is not valid
     } else {
-      
+        echo "<h1>please fill the form</h1>";
     }
 } else {
-   
+    echo "<h1>please fill the form</h1>";
 }
